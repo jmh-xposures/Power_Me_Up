d12232f8ada9bf04ac51173f2167297aaad73216
Juan-Miguel Hernandez <jmh@jmhernandez.com>
2020-08-18 01:10:12 -0400

    added most actions... still need some advice for some actions from TA

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,35 @@
 class RentalsController < ApplicationController
+
+  def new
+    @rental = Rental.new
+  end
+
+  def create
+    @rental = Rental.new(rental_params)
+    # @power = Power.find(params[:power_id])
+    #By permitting it in the rental_params power_id is included in the rental params
+    # @rental.power_id = @power.id
+    @rental.user = current_user
+
+    if @rental.save
+      flash.notice = "Thank you for booking this power!"
+      redirect_to power_path(@rental.power)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @rental = Rental.find(params[:id])
+    #should I have two methods for each button (accept or reject) instead of this method
+
+  end
+
+
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:content, :power_id)
+  end
 end
