dcc816f4d42cfcdf8631cb88be65555c749218e5
Junior Rivas <jrtimmobilier@gmail.com>
2020-08-19 12:40:04 -0400

    hero human profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,12 @@ class PagesController < ApplicationController
 
   def profile
   	if current_user.is_hero 
+  		@number_of_pending_rentals = Rental.joins(:power).where(status: nil , powers: {user: current_user}).count 
   	   render "hero" 
   	 else 
   	   render "human"   
   	end	
+
   end
 
   def home 
